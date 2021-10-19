5df9ce7f767d64cc73aba15e0396ee4db64800b6
Oleg <obarnev@inbox.ru>
2021-10-19 10:12:31 +0300

    develop init

--- frontend-callendar/src/App.js
+++ frontend-callendar/src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello123</h1>
+      <h1>Hello develop</h1>
       <ul>
         {people.map(p => (
           <li key={p.id}>{p.name}</li>
